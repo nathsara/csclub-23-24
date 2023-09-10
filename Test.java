c1dbf886105060f0105c339b2ad0a9b008adfa61
nathsara <saranath2006@gmail.com>
2023-09-10 18:12:32 -0500

    Create Test.java
    test file

---
+++ Test.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner stdin = new Scanner(System.in);
+        int num = stdin.nextInt();
+        System.out.println(num);
+        stdin.close();
+    }
+}
